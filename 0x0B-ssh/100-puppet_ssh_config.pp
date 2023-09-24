c7cdc75695e9af0cd92ca513a536244039f8dd98
loay <loaywaleed9@gmail.com>
2023-09-25 00:11:18 +0300

    Testing

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,10 +1,11 @@
 # puppet config file
-file_line {'disable password auth':
-  path => '/etc/ssh/ssh_config',
-  line => 'PasswordAuthentication no'
-}
 file_line {'IdentityFile':
   ensure => 'present'
   path   => '/etc/ssh/ssh_config',
   line   => 'IdentityFile ~/.ssh/school'
 }
+file_line {'disable password auth':
+  path => '/etc/ssh/ssh_config',
+  line => 'PasswordAuthentication no'
+}
+
