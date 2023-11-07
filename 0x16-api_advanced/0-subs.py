f7bfc13bbad935b3a150d4d85a9519c5fd80d7f8
= <=>
2023-11-07 22:38:48 +0200

    getting subscribers count from a subreddit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""A script that gets subscriber count of a subreddit"""
+
+
+def number_of_subscribers(subreddit):
+    import requests
+    user_agent = {"User-Agent": "anon"}
+    api_response = requests.get(
+        "https://www.reddit.com/r/{}/about.json".format(subreddit), headers=user_agent, allow_redirects=False)
+
+    if api_response.status_code == 200:
+        return api_response.json().get("data").get("subscribers")
+
+    return 0
