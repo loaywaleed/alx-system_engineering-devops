f13e405ea7cee3c540dff27516bee9e07d5e2dd5
= <=>
2023-11-07 22:39:27 +0200

    pycodestyle

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,7 +6,8 @@ def number_of_subscribers(subreddit):
     import requests
     user_agent = {"User-Agent": "anon"}
     api_response = requests.get(
-        "https://www.reddit.com/r/{}/about.json".format(subreddit), headers=user_agent, allow_redirects=False)
+        "https://www.reddit.com/r/{}/about.json".format(subreddit),
+        headers=user_agent, allow_redirects=False)
 
     if api_response.status_code == 200:
         return api_response.json().get("data").get("subscribers")
