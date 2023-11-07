beb64a6cd2bb3a5de468965f36a7ce6e577d9f15
= <=>
2023-11-08 00:13:14 +0200

    added recursive function to obtain all hot posts

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Top ten hot posts for a given subreddit"""
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    """Gets top ten hot posts in a subreddit"""
+    import requests
+
+    user_agent = {"User-Agent": "anon"}
+
+    api_response = requests.get(
+        "https://www.reddit.com//r/{}/hot.json".format(
+            subreddit),
+        params={"after": after},
+        headers=user_agent, allow_redirects=False)
+
+    if api_response.status_code == 200:
+        [hot_list.append(items.get("data").get("title"))
+         for items in api_response.json().get("data").get("children")]
+        after = api_response.json().get("data").get("after")
+        if not after:
+            return hot_list
+
+        return recurse(subreddit, hot_list, after)
+
+    else:
+        return None
